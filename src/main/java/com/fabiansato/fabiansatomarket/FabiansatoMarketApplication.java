96fd1835959faafce59140acd10922a4ecb94b3a
fabiansato@gmail.com <40687901+fabiansato@users.noreply.github.com>
2022-03-16 01:11:32 -0300

    novedades

---
+++ src/main/java/com/fabiansato/fabiansatomarket/FabiansatoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.fabiansato.fabiansatomarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabiansatoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabiansatoMarketApplication.class, args);
+	}
+
+}
