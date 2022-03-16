0c4af35d5cd99cb9f0c734f2a408dfe6b36b3363
fabiansato@gmail.com <40687901+fabiansato@users.noreply.github.com>
2022-03-16 00:10:57 -0300

    testing en spring

---
+++ src/test/java/com/fabiansato/fabiansatomarket/FabiansatoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiansato.fabiansatomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabiansatoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
